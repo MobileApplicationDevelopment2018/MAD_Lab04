70b061bd14930a8a6e8c564ac5510168db7f45cb
nunzio-turco <nunxyo@gmail.com>
2018-05-14 13:03:09 +0200

    Prototype of a Splash Screen

---
+++ app/src/main/java/it/polito/mad/mad2018/OnboardingActivity.java
@@ -0,0 +1,56 @@
+package it.polito.mad.mad2018;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class OnboardingActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        RelativeLayout linearLayout = findViewById(R.id.splash);
+        ImageView imageView = findViewById(R.id.splash_image);
+        TextView textView = findViewById(R.id.splash_text);
+        Animation animation_image = AnimationUtils.loadAnimation(this, R.anim.from_top);
+        Animation animation_text = AnimationUtils.loadAnimation(this, R.anim.from_bottom);
+
+        imageView.setAnimation(animation_image);
+        textView.setAnimation(animation_text);
+
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callMainActivity();
+                finish();
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                callMainActivity();
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+    private void callMainActivity(){
+        Intent callMainActivity = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(callMainActivity);
+    }
+
+
+}
